31825ca7647736f469c4bfe823028f180e973251
Daniel Stellmon <Dstellm@gmail.com>
2025-05-07 01:43:01 -0500

    It works now

--- Client/min.py
+++ Client/min.py
@@ -2,6 +2,9 @@ import requests
 import yaml
 import os
 import subprocess
+import logging
+import paramiko
+from scp import SCPClient
 
 def processIP(response):
     responseList = response.text.splitlines()
@@ -14,13 +17,48 @@ def processIP(response):
 def compareIP(currentIP, settingsDict):
     if currentIP != settingsDict["wanIP"]:
         settingsDict["wanIP"] = currentIP
-        updateAndSendSettings(settingsDict)
+        update(settingsDict)
+        pushUpdate(settingsDict)
 
-def updateAndSendSettings(settingsDict):
+def update(settingsDict):
     with open(settingsFile, 'w') as file:
         yaml.dump(settingsDict, file)
-        p = subprocess.Popen(["scp", settingsFile, "zyzyx@barqu.xyz:/home/zyzyx/test.txt"])
-        sts = os.waitpid(p.pid, 0)
+    # print("update")
+
+def pushUpdate(settingsDict):
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+        try:
+            # Load the SSH agent
+            agent = paramiko.Agent()
+            keys = agent.get_keys()
+            
+            if not keys:
+                raise Exception("No keys found in SSH agent")
+            
+            # Try each key in the agent
+            for key in keys:
+                try:
+                    ssh.connect(hostname=settingsDict['host'], username=settingsDict['userName'], pkey=key, timeout=30)
+                    break
+                except paramiko.AuthenticationException:
+                    continue
+            else:
+                raise Exception("No key worked for authentication")
+            
+            # Initialize the SCP client
+            with SCPClient(ssh.get_transport()) as scp:
+                # Upload a file from local to remote
+                local_file = 'settings.yml'
+                remote_file = settingsDict['output']
+                scp.put(local_file, remote_file)
+                print(f"File {local_file} uploaded successfully to {remote_file}")
+        
+        except Exception as e:
+            print("An error occurred:", e)
+        finally:
+            print("Connection closed")
 
 settingsFile = os.path.join(os.getcwd(), "settings.yml")
 with open(settingsFile, 'r') as file:
